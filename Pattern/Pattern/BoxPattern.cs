e8ba159ee818810612287ce7966d024cd34696e1
BalikiriSudheer <sudheerbalikiri2000@gmail.com>
2023-09-25 19:51:15 +0530

    [Sudheer]Add:ReverseTriangle

---
+++ Pattern/Pattern/BoxPattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    internal class BoxPattern
+    {
+        public void Assign(int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= n; j++)
+                {
+                    if (i == 1 || i == n || j==1 || j == n)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                   
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Pattern/Pattern/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    internal class Program
+    {
+        private static bool intersted;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the problem Numbers:"+
+                "\n 1.Pattern.\n"+
+                "\n 2.BoxPattern.\n 0---QUIT");
+                int choice =int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the number of lines to print the triangle");
+                    int num = int.Parse(Console.ReadLine());
+                    Pattern p = new Pattern();
+                    p.Test(num);
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the rows");
+                    int n = int.Parse(Console.ReadLine());
+                    BoxPattern bp = new BoxPattern();
+                    bp.Assign(n);
+                    Console.ReadLine();
+                    break;
+                case 0:
+                    if (choice == 0)
+                    {
+                        intersted = false;
+                        Console.WriteLine("Thanks for your interst");
+                    }
+                    break;
+            }
+        }
+    }
+}
